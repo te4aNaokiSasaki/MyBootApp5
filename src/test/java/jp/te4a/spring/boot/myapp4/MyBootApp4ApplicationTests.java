af2d9de4747e85757bee0853ce6539eb97e261d3
佐々木 尚輝 <180574@jc-21.jp>
2021-05-27 15:05:10 +0900

    kadai5

---
+++ src/test/java/jp/te4a/spring/boot/myapp4/MyBootApp4ApplicationTests.java
@@ -0,0 +1,22 @@
+package jp.te4a.spring.boot.myapp4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import jp.te4a.spring.boot.myapp5.HelloController;
+
+
+
+public class MyBootApp4ApplicationTests {
+
+//	@Test
+//    public void test_text()
+//    {
+//    String text = "this is Spring Boot sample.";
+//    HelloController helloController = new HelloController();
+//    String actual = helloController.index();
+//    assertEquals(text,actual);
+//    }
+
+}
